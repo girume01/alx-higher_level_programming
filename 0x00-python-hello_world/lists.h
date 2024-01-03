cfe083b9ca16d221194b4c49065652dbb27e91c5
girum <girumassefa6@gmail.com>
2024-01-03 01:16:00 -0800

    0x00 python 07 task

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,2 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
+/**
+ * struct listint_s - single linked list
+ * @n: intiger number
+ * @next: points to the next node
+ * Description: singly linked list node structure for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 size_t print(const list_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif
