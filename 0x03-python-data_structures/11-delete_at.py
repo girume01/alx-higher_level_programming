d8ead3032c772fa4dc955afe343b2217d66d3767
girum <girumassefa6@gmail.com>
2024-01-08 22:27:43 -0800

    0x03 python task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    #check if the index is valid
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    #create new list
+    new_list = my_list[:idx] + my_list[idx+1:]
+
+    return new_list
+
