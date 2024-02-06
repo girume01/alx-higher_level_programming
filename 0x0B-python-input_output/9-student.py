34d26b01f3ef9677b16f026fb1e35dd53fdaed00
girum <girumassefa6@gmail.com>
2024-02-06 09:17:13 -0800

    0x0B python task 09

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,17 +3,16 @@
 
 class Student:
     """
-    Represent a student.
-    """
+    Represent a student."""
 
     def __init__(self, first_name, last_name, age):
         """
         initialize a new Student.
 
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int): age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -21,6 +20,5 @@ class Student:
 
     def to_json(self):
         """
-        get a dictionary representation of the student.
-        """
+        Get a dictionary representation of the Student."""
         return self.__dict__
