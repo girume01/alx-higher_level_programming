a65affd6f8d348062abd3730e80335a46d455a79
girum <girumassefa6@gmail.com>
2024-02-05 17:35:50 -0800

    0x0A python task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define the class rectangle that inherits from besegeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """
+        Intialize Rectangle.
+
+        Args:
+            width (int): width of the Rectangle.
+            height (int): height of the Rectangle.
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
