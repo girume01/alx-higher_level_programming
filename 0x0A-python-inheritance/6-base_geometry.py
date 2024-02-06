c92d6a3b882f1783591b3f423634668bcbaeaf6d
girum <girumassefa6@gmail.com>
2024-02-05 16:29:41 -0800

    0x0A python task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a bas geometry class basegeometry.
+"""
+
+class BaseGeometry:
+    """
+    Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+
