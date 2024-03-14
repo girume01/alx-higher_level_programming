3dfbbee201d7e0b26fd52c224aad2e6ee368a1ae
girum <girumassefa6@gmail.com>
2024-03-14 16:04:16 -0700

    0x0d sql tasks

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computes the average score of  records in the table second_table in my MySQL server
+
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Lists rows of the table first_table in my MySQL server
+
+SELECT * FROM `first_table`;
