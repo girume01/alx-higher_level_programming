e0b49d2212b21b662904bfdb3a995e0fd62fef68
girum <girumassefa6@gmail.com>
2024-04-26 07:11:59 -0700

    0x11 python network 1

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+    Print error code in utf-8
+"""
+import urllib.request
+import urllib.parse
+from urllib.error import URLError, HTTPError
+from sys import argv
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode(encoding="utf-8"))
+    except URLError as e:
+        print("Error code: {}".format(e.code))
