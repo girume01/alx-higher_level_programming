757b6458126e77d40fc4a23d4d151d93bfea553f
girum <girumassefa6@gmail.com>
2024-05-21 14:12:39 +0000

    0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+t fs = require('fs');
+
+const file = process.argv[2] || '';
+const data = process.argv[3] || '';
+
+fs.writeFile(file, data, 'utf8', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
